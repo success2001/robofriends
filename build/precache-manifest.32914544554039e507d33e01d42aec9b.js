8c86d170c5ab22a908aaa387d6e22f8e468a4b6b
success2001 <50456303+success2001@users.noreply.github.com>
2021-10-12 03:13:31 -0700

    Add files via upload

---
+++ build/precache-manifest.32914544554039e507d33e01d42aec9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd1eaccf3bf5d936235b873c4f21d4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ebc0b4e843ba40ee5655",
+    "url": "/static/css/main.60281e1d.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ebc0b4e843ba40ee5655",
+    "url": "/static/js/main.c4731fd5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
